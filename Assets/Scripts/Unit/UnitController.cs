dd54ca114d2435e672c41b71a5bec58304f5e8e3
YamamiyaShota <153970961+YamamiyaShota@users.noreply.github.com>
2025-08-26 17:19:47 +0900

    範囲選択でUnitを複数選択できるようにした。

--- Assets/Scripts/Unit/UnitController.cs
+++ Assets/Scripts/Unit/UnitController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
@@ -7,22 +8,71 @@ public class UnitController : MonoBehaviour
     [SerializeField] private Camera _camera;
     [SerializeField] private RaycastHit _raycastHit;
 
-    private NavMeshAgent _selectUnit;
+    [SerializeField] private List<NavMeshAgent> _selectUnits = new();
+    private NavMeshAgent[] _agents;
+    [SerializeField]private NavMeshAgent _selectUnit;
     private Vector3 _targetPosition;
+
+    private Vector2 _startMousePosition;
+    private Vector2 _endMousePosition;
+
+    private Rect selectionRect;
+
+    private void Start()
+    {
+        _agents = FindObjectsByType<NavMeshAgent>(FindObjectsSortMode.InstanceID);
+    }
 
     public void OnClick(InputAction.CallbackContext context)
     {
+        if (!context.performed)
+        {
+            return;
+        }
         Ray ray = _camera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        _startMousePosition = Mouse.current.position.ReadValue();
         if (Physics.Raycast(ray, out _raycastHit))
         {
             switch (_raycastHit.collider.gameObject.tag)
             {
-                case "Player":
-                    _selectUnit = _raycastHit.collider.gameObject.GetComponent<NavMeshAgent>();
-                    break;
+                //case "Player":
+                //    _selectUnit = _raycastHit.collider.gameObject.GetComponent<NavMeshAgent>();
+                //    break;
                 case "Ground":
                     MoveTargetPosition(_raycastHit);
                     break;
+            }
+        }
+    }
+
+    public void OnRelease(InputAction.CallbackContext context)
+    {
+        if (!context.performed)
+        {
+            return;
+        }
+
+        _endMousePosition = Mouse.current.position.ReadValue();
+        float x = Mathf.Min(_startMousePosition.x, _endMousePosition.x);
+        float y = Mathf.Min(_startMousePosition.y, _endMousePosition.y);
+
+        float width = Mathf.Abs(_startMousePosition.x - _endMousePosition.x);
+        float height = Mathf.Abs(_startMousePosition.y - _endMousePosition.y);
+
+        selectionRect = new Rect(x, y, width, height);
+
+        FIndSelectionsUnit(selectionRect);
+    }
+
+    private void FIndSelectionsUnit(Rect selection)
+    {
+        _selectUnits.Clear();
+        foreach (var agent in _agents)
+        {
+            var screenPos = _camera.WorldToScreenPoint(agent.transform.position);
+            if (selection.Contains(new Vector2(screenPos.x, screenPos.y)))
+            {
+                _selectUnits.Add(agent);
             }
         }
     }
@@ -41,6 +91,24 @@ public class UnitController : MonoBehaviour
         {
             _targetPosition.y = _selectUnit.transform.position.y;
             _selectUnit.SetDestination(_targetPosition);
+            _selectUnit = null;
+        }
+
+        if(_selectUnits.Count > 0)
+        {
+            foreach(var agent in _selectUnits)
+            {
+                _targetPosition.y = agent.transform.position.y;
+                agent.SetDestination(_targetPosition);
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GUI.Box(selectionRect, "");
         }
     }
 }
