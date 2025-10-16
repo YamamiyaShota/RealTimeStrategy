ddc38b50494c41fae052511f8ec2c4c2b18195a9
YamamiyaShota <153970961+YamamiyaShota@users.noreply.github.com>
2025-10-16 15:11:18 +0900

    [update] AddDamageに戻り値を設定し、コメントを追加

--- Assets/Scripts/Interface/IUnit.cs
+++ Assets/Scripts/Interface/IUnit.cs
@@ -3,8 +3,13 @@
     public int HP { get; }
     public int ATK { get; }
     public int DF { get; }
-    public float AttackInterval { get; }
     public float MoveSpeed { get; }
+    public float AttackInterval { get; }
     public float AttackRange { get; }
-    public void AddDamage(int atk);
+    /// <summary>
+    /// ダメージ処理
+    /// </summary>
+    /// <param name="atk">攻撃力</param>
+    /// <returns>trueなら死亡,falseなら生存</returns>
+    public bool AddDamage(int atk);
 }
