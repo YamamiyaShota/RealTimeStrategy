595972e7d78109d9dbc845d442f1dc3b4ef8699f
YamamiyaShota <153970961+YamamiyaShota@users.noreply.github.com>
2025-10-16 14:42:31 +0900

    [add] IStateの実装

---
+++ Assets/Scripts/Interface/IState.cs
@@ -0,0 +1,23 @@
+﻿public interface IState
+{
+    public StateType GetCurrentState { get; }
+    public StateType GetNextState { get; }
+    /// <summary>
+    /// ステートを設定する
+    /// </summary>
+    /// <param name="currentState">今の状態</param>
+    /// <param name="nextState">次の状態</param>
+    public void SetState(StateType currentState, StateType nextState = StateType.Idle);
+    public void OnStateBegine();
+    public void OnStateEnd();
+
+    public void Update();
+}
+
+public enum StateType
+{
+    Idle,
+    Move,
+    Attack,
+    Death,
+}
