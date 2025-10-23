98045bc6b57c576906f279bec02c5ab9aa1a4dd7
YamamiyaShota <153970961+YamamiyaShota@users.noreply.github.com>
2025-10-23 14:14:35 +0900

    [add] EnemyTargetDetectorの追加、Enemyフォルダーを追加

---
+++ Assets/Scripts/Enemy/EnemyTargetDetector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTargetDetector : MonoBehaviour
+{
+    [SerializeField] private Enemy _enemy;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            IUnit unit = other.gameObject.GetComponentInParent<IUnit>();
+            _enemy.SetAttackTarget(other.gameObject.transform.position, unit);
+        }
+    }
+}
